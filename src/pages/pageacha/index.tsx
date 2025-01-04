9f2e5feb8b74c2fedaea5784f2f03bab9a230d33
mateus <wallassv2@gmail.com>
2025-01-04 06:50:18 -0300

    adding header e search

--- src/pages/pageacha/index.tsx
+++ src/pages/pageacha/index.tsx
@@ -2,13 +2,30 @@ import { AchaContainerMain } from "./styles"
 import logolola from "../../images/logolola.jpg"
 
 
+
+
 export function PageAcha() {
 
     return (
 
         <AchaContainerMain>
+            {/* cabeçalho  */}
+            <div id="header" >
+                <div id="contentheader">
+                   <img id="logolola" src={logolola} alt="" />
+
+                   {/* barra de pesquisa */}
+                    <form id="search-bar">
+                         <input id="inputsearch" type="text" autoComplete="on" placeholder="Encontre seu produto aqui" />
+                         <button aria-labelledby="Buscar" type="submit">  
+                           <svg stroke="currentColor" fill="currentcolor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></svg> 
+                         </button>
+                         
+                    </form>
+                </div>
+            </div>
+            
             <div id="box2acha" >
-                <img id="logolola" src={logolola} alt="" />
               <h1> heloww </h1>
             </div>
 
--- src/pages/pageacha/styles.ts
+++ src/pages/pageacha/styles.ts
@@ -39,5 +39,128 @@ export const AchaContainerMain = styled.main`
 
    }
 
+   #header {
+      display: flex;
+      max-width: 100%;
+      max-height: 100%;
+      width: 100%;
+      height: 5.70rem;
+      background-color: #222222;
+      box-sizing: border-box;
+
+      
+   }
+
+   #contentheader {
+      display: flex;
+      max-width:1340px;
+      width:100%;
+      align-items: center;
+      box-sizing: border-box;
+      margin: auto;
+
+
+   }
+
+   #search-bar {
+      position: relative;
+      box-sizing: border-box;
+      display: block;
+      flex-grow: 3;
+      height: auto;
+      margin-bottom: auto;
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: auto;
+      max-width: 650px;
+      padding-bottom: 20px;
+      padding-left: 20px;
+      padding-right: 20px;
+      padding-top: 20px;
+      width: 100%;
+      font-family: Roboto, sans-serif;
+      color: rgba(254, 254, 254, 0.667);
+      unicode-bidi: isolate;
+      
+         svg {
+            overflow-clip-margin: content-box;
+            overflow: hidden;
+
+             color: blue;
+         } 
+}
+   #inputsearch {
+   
+      box-sizing: border-box;
+      display: inline-block;
+      height: 50px;
+      overflow-x: clip;
+      overflow-y: clip;
+      padding-bottom: 15px;
+      padding-left: 15px;
+      padding-right: 15px;
+      padding-top: 15px;
+      width: 650px;
+      font-family: Poppins, sans-serif;
+      font-feature-settings: normal;
+      font-size: 14px;
+      font-size-adjust: none;
+      font-stretch: 100%;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 14px;
+      text-align: start;
+      text-indent: 0px;
+      text-shadow: none;
+      text-transform: none;
+      word-spacing: 0px;
+      color: rgb(34, 34, 34);
+      background-color: rgb(255, 255, 255);
+      border-bottom-style: solid;
+      border-bottom-width: 1px;
+      border-image-outset: 0;
+      border-image-repeat: stretch;
+      border-image-slice: 100%;
+      border-image-source: none;
+      border-image-width: 1;
+      border-left-color: rgba(0, 0, 0, 0);
+      border-left-style: solid;
+      border-left-width: 1px;
+      border-right-color: rgba(0, 0, 0, 0);
+      border-right-style: solid;
+      border-right-width: 1px;
+      border-top-color: rgba(0, 0, 0, 0);
+      border-top-style: solid;
+      border-top-width: 1px;
+      cursor: text;
+      transition-delay: 0s;
+      transition-duration: 0.3s;
+      transition-property: all;
+      transition-timing-function: ease;
+      appearance: auto;
+      text-rendering: auto;
+      border-bottom-left-radius: 12px;
+      border-bottom-right-radius: 12px;
+      border-top-left-radius: 12px;
+      border-top-right-radius: 12px;
+      overflow-clip-margin: 0px;
+      padding-block-end: 15px;
+      padding-block-start: 15px;
+      padding-inline-end: 15px;
+      padding-inline-start: 15px;
+      -webkit-rtl-ordering: logical;
+   }
+  
+   
+   #logolola {
+      display: block;
+      border-top-left-radius: 9999px;
+      border-top-right-radius: 9999px;
+      border-bottom-left-radius: 9999px;
+      border-bottom-right-radius: 9999px;
+      max-width: 98px;
+      max-height: 98px;
+
+   }
 
 ` 
